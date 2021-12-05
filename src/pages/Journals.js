a5c6cffcb948e71ba8a13b6e759e68c491aa69b5
LuisFalci <78694561+LuisFalci@users.noreply.github.com>
2021-12-05 12:48:50 -0300

    Teste lightbox

--- src/pages/Journals.js
+++ src/pages/Journals.js
@@ -1,50 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import { DataGrid } from '@mui/x-data-grid';
+import Lightbox from "react-awesome-lightbox";
+// You need to import the CSS only once
+import "react-awesome-lightbox/build/style.css";
+import foto from '../img/testeimage.png'
+import foto2 from '../img/actualidade.jpg'
 
 const Journals = () => {
+  const element = "Hemeroteca";
 
-	const element = 'Hemeroteca';
+  fetch("http://127.0.0.1:8000/api/journals", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then(({ data }) => results(data));
 
-	fetch('http://127.0.0.1:8000/api/journals',
+  function imagem() {
+	const image = [];
+	const lista = [
 		{
-			method:"GET",
-			headers:
-			{
-				"Content-Type" : "application/json",
-			}
-		})
-		.then((response) => response.json())
-		.then(({data}) => results(data));
+			url:foto,
+			title:"image title 1"
+		},
+		{
+			url:foto2,
+			title:"image title 2"
+		}
+	]
 
-	function results(data)
-	{
-		const content = []
+	image.push(
+		<Lightbox
+		images={lista}
+		title="fotos"	
+		onClose={() => Journals()}
+	  />
+	)
+	return ReactDOM.render(image, document.getElementById("appView"));
+  }
 
-		content.push(
-			<table key="1">
-				<tbody>
-					{
-						data.map((key, value) =>
-						{
-							return(
-							<tr key={value}>
-								<td>
-								{key.attributes.title}
-								</td>
-							</tr>)
-						})
-					}
-				</tbody>
-			</table>
-			)
+  function results(data) {
+    const content = [];
 
-		return (
-			ReactDOM.render(content, document.getElementById('appView'))
-		);
+    content.push(
+      <>
+	  
+        <button onClick={() => imagem()}>Imagem</button>
 
-	}
+        <table key="1">
+          <tbody>
+            {data.map((key, value) => {
+              return (
+                <tr key={value}>
+                  <td>{key.attributes.title}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
 
-}
+    return ReactDOM.render(content, document.getElementById("appView"));
+  }
+};
 
 export default Journals;
