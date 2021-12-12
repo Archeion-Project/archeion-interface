9d660fc2d5b6e97def971ba0f68723e5f4980a53
Giovanni Stroppa <giosfdev@gmail.com>
2021-12-12 12:05:12 -0300

    Shows journal information on Jounal page

--- src/pages/journals/JournalYears.js
+++ src/pages/journals/JournalYears.js
@@ -8,6 +8,8 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Grid from '@mui/material/Grid';
 import JournalMonthsYear from './JournalMonthsYear.js'
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
 
 const JournalYears = (id) => {
 
@@ -22,42 +24,77 @@ const JournalYears = (id) => {
 		.then((response) => response.json())
 		.then((value) => results(value))
 
+
 	function results(data)
 	{
 		const content = []
 		content.push(
-			<Box sx={{ flexGrow: 1 }}>
-				<Grid container spacing={1}>
-					<Grid id="year" item xs={3}>
-						<FormControl sx={{ m: 1, minWidth: 120 }}>
-							<InputLabel id="journal-years">Ano</InputLabel>
-							<Select
-								labelId="journal-years"
-								id="journal-years"
-								label="Ano"
-								onChange={(year) => getMonthsYear(data.journal.id, year.target.value)}
-							>
-								{data.years.map((key, value) => {
-									return (
-										<MenuItem value={key}>{key}</MenuItem>
-										)
-									})
-								}
-								</Select>
-						</FormControl>
+			<Grid>
+				{/* <FormControl sx={{ width: 600 }}> */}
+					{/* <TextField
+						id="journal-title"
+						label="Nome do Jornal"
+						color="success"
+						variant="standard"
+						value={data.journal.title + ' - ' + data.journal.localization}/> */}
+				{/* </FormControl> */}
+				<h2>Nome do Periódico: {data.journal.title + ' - ' + data.journal.localization}</h2>
+				<h4>Período do acervo: {data.startDate + ' - ' + data.endDate}</h4>
+				{/* <FormControl sx={{ width: 600 }}> */}
+					{/* <TextField
+						id="journal-title"
+						label="Disponibilidade do Acervo"
+						color="success"
+						variant="standard"
+						value={data.startDate + ' - ' + data.endDate}/> */}
+				{/* </FormControl> */}
+				{/* <br />
+				<br /> */}
+				<Divider />
+				<h4>Escolha uma Edição</h4>
+				<Box sx={{ flexGrow: 1 }}>
+					<Grid container item xs={12}>
+						<Grid id="year" item xs={2}>
+							<FormControl sx={{ m: 1, minWidth: 120 }}>
+								<InputLabel id="journal-years">Ano</InputLabel>
+								<Select
+									labelId="journal-years"
+									id="journal-years"
+									label="Ano"
+									onChange={(year) => getMonthsYear(data.journal.id, year.target.value)}
+								>
+									{data.years.map((key, value) => {
+										return (
+											<MenuItem
+												key={'year' + key}
+												value={key}
+											>{key}</MenuItem>
+											)
+										})
+									}
+									</Select>
+							</FormControl>
+						</Grid>
+						<Grid id="month" item xs={2}>
+						</Grid>
+						<Grid id="issue" item xs={2}>
+						</Grid>
+						<Grid id="page" item xs={2}>
+						</Grid>
 					</Grid>
-					<Grid id="month" item xs={3}>
+					<Grid>
+						<div id="page-viewer"></div>
 					</Grid>
-					<Grid id="issue" item xs={3}>
+				</Box>
+				<br />
+				<Divider />
+				<br />
+				<Box>
+					<Grid>
+						<div id="citation-viewer"></div>
 					</Grid>
-					<Grid id="page" item xs={3}>
-					</Grid>
-				</Grid>
-				<Grid>
-					<div id="page-viewer"></div>
-				</Grid>
-
-			</Box>
+				</Box>
+			</Grid>
 		)
 		content.push(
 			<Box>
